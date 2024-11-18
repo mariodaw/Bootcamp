3dedc4153223f0ecde2371e8158ebf6451c7f799
Mario Tomas <94860126+mariodaw@users.noreply.github.com>
2024-11-18 07:48:39 +0100

    Delete node directory

--- node/ej1/index.js
+++
@@ -1,1 +0,0 @@
-console.log('Mi primer script en The Bridge!!!')
--- node/ej3/index.js
+++
@@ -1,5 +0,0 @@
-const math = require('./numeros');
-const Arr = [2, 3, 101, 201, 202, 100]
-for (let i = 0; i < Arr.length; i++) {
-    console.log(math.esPar(Arr[i]));
-}
